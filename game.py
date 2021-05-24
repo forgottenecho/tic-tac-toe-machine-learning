7504e5f6089effa845c2eb60c831f298a332168e
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2021-05-24 13:58:57 -0500

    readability improvement

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import numpy as np
 import time
 
+# returns an integer representing game status, nonzero if game is over
 def game_over(state):
+    # tallies move count for each playable lane
     max_in_a_row = get_lanes(state).sum(axis=-1).max()
     min_in_a_row = get_lanes(state).sum(axis=-1).min()
+    
     if max_in_a_row == 3:
         print('x won')
         return 1
@@ -14,37 +17,41 @@ def game_over(state):
         print('tie game')
         return 3
     return 0
-    
+
+# chops 3x3 game board up into eight lanes    
 def get_lanes(state):
     lanes = np.zeros((8,3))
-    lanes[:3] = state
-    lanes[3:6] = state.transpose()
-    lanes[6] = state.diagonal()
-    lanes[7] = np.diagonal(np.fliplr(state))
+    lanes[:3] = state # rows
+    lanes[3:6] = state.transpose() # columns
+    lanes[6] = state.diagonal() # positive diagonal
+    lanes[7] = np.diagonal(np.fliplr(state)) # negative diagonal
     return lanes
 
+# finds possible moves given board state and picks highest value one according to model
 def play(model, state):
     possible_states = None
+    
+    # loop over all nine board spots
     for i in range(3):
         for j in range(3):
-            # print(state[i,j])
+            # skip played on spots
             if not state[i,j] == 0: continue
-            # print('found one')
             
+            # hypothetically play the empty spot
             state[i,j] = 1
             if possible_states is None:
                 possible_states = state.copy().reshape((1,3,3))
             else:
                 possible_states = np.concatenate([possible_states, state.reshape((1,3,3))])
             state[i,j] = 0
-            # print(possible_states)
-    # print('found {} possible_states'.format(possible_states.shape[0]))
-    # print(possible_states)
+    
     possible_states = possible_states.reshape((-1,3,3))
     values = model.predict(possible_states)
-    # print(values)
+    
+    # select most optimal move
     return possible_states[values.argmax()]
 
+# linear model of board value
 class Model():
     def __init__(self, W=None, b=None):
         if W is None:
@@ -59,28 +66,40 @@ class Model():
         print(self.W)
         
     def predict(self, possible_states):
+        # initialize prediction array
         size = possible_states.shape[0]
         y = np.zeros(size)
+        
+        # fill with predictions from linear model
         for i in range(size):
             x = self.get_features(possible_states[i])
             y[i] = self.W.dot(x) + self.b
         return y
-        
+    
+    # model class decides how to perform feature extraction from the board
     def get_features(self, state):
         lanes = get_lanes(state)
         features = np.zeros((4))
+        
         for lane in lanes:
             ours = 0
             opps = 0
+            
+            # count plays in lane
             for v in lane:
                 if v == 1: ours += 1
                 if v == -1: opps += 1
+                
+            # we have two in a row, three in a row
             if ours == 2: features[0] += 1
             if ours == 3: features[1] += 1
+            
+            # they have two in a row, three in a row
             if opps == 2: features[2] += 1
             if opps == 3: features[3] += 1
         return features
-    
+
+    # gradient descent on squared error
     def fit(self, train_states, ytrain, lr=0.01):
         print('training model')
         for i in range(train_states.shape[0]):
@@ -93,18 +112,22 @@ class Model():
             print(self.b)
         print('done training')
         
-
+# generates training data given a matrix of chronological game states
 def critic(trace, model):
+    # flip is used to train on opponents data
     flip = -1
+    
     xtrain = np.zeros((trace.shape[0]-1,3,3))
     ytrain = np.zeros((trace.shape[0]-1))
     for i in range(trace.shape[0]-1):
-        flip *= -1
+        # makes all states look like x's perspective
+        flip *= -1        
         xtrain[i] = flip*trace[i]
         
         value = None
         type = game_over(flip*trace[i+1])
         if type == 0:
+            # training value is set to our current prediction of NEXT state
             value = model.predict(flip*trace[i+1].reshape(1,3,3))
         elif type == 1:
             # you won
@@ -118,16 +141,19 @@ def critic(trace, model):
         ytrain[i] = value
     return xtrain, ytrain
     
-
+# this main is rather botched together but it's mostly PoC for me
 if __name__ == '__main__':
-    #optimal model after 50k games
-    model = Model(np.array([2.76e-13,5.66e-03,-5.499e+01,-2.284e-01]), 99.9)
+    # create two models to play against each other
+    model = Model(np.array([2.76e-13,5.66e-03,-5.499e+01,-2.284e-01]), 99.9) # optimal model after 50k games
     # model = Model()
     model_frozen = Model()
     
+    # metrics
     won = 0.0
     total = 0.0
     ties = 0.0
+    
+    # train forever
     while True:
         # time.sleep(5)
         total += 1
@@ -137,10 +163,12 @@ if __name__ == '__main__':
         
         trace = state.copy().reshape(1,3,3)
         while True:
+            # x plays
             state = play(model, state)
             trace = np.concatenate([trace,state.reshape(1,3,3)])
             print(state)
             
+            # check game over
             type = game_over(state)
             if type:
                 print('game over' +str(type))
@@ -150,13 +178,13 @@ if __name__ == '__main__':
                     ties += 1
                 break
                 
-            # flip *= -1
+            # o plays
             state = flip*play(model_frozen, flip*state)
             trace = np.concatenate([trace,state.reshape(1,3,3)])
-            
             print(state)
-            # time.sleep(25)
+            time.sleep(25)
             
+            # check game
             type = game_over(state)
             if type:
                 print('game over' +str(type))
@@ -166,13 +194,18 @@ if __name__ == '__main__':
                     ties += 1
                 break
         
+        # the game has ended
         print('final state')
         print(state)  
+        
+        # train model on this game
         xtrain, ytrain = critic(trace, model)
         model.fit(xtrain, ytrain)
-        print(won)
-        print(ties)
-        print(total)
-        print(float(won)/total)
+        
+        # print metrics
+        print('Total games you have won: {}'.format(won))
+        print('Total ties: {}'.format(ties))
+        print('Total games you have played: {}'.format(total))
+        print('Win rate: {}'.format(float(won)/total))
         if total%500 == 0:
             time.sleep(2)
